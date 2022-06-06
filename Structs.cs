409fabfe309e2ec588e16c2c10e5118f7af4bb4e
Jarno Montonen <jarno.montonen@gmail.com>
2022-06-06 14:24:56 +0300

    Add project files.

---
+++ Structs.cs
@@ -0,0 +1,12 @@
+﻿namespace TypeLoadError;
+
+public readonly struct One {
+    public readonly Generic<Two> Child;
+}
+
+public readonly struct Two {
+    // Commenting this gets rid of the TypeLoadException
+    public readonly One Child;
+}
+
+public readonly struct Generic<T> { }
